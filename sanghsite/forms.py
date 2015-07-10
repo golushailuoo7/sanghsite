b10d3e702502f45d7464baa86cf7b39e29666cad
golushailuoo7 <golushailuoo7@gmail.com>
2015-07-10 22:13:53 +0530

    added signup

--- sanghsite/forms.py
+++ sanghsite/forms.py
@@ -3,10 +3,17 @@ from django.contrib.auth.models import User
 
 
 class SignupForm(forms.ModelForm):
-    confirm_password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput(), required=True)
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        # Making name required
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['email'].required = True
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'password']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password']
         widgets = {
             'password': forms.PasswordInput(),
         }
--- sanghsite/views.py
+++ sanghsite/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
 from gbtaluka.models import UserDetail
+from django.contrib.auth.models import User
 
 def index_view(request):
     return render(request, 'index.html', {})
@@ -8,11 +9,18 @@ def index_view(request):
 def signup_view(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
-        if form.is_valid:
-            user = form.save()
-            user_detail = UserDetail(user=user)
-            user_detail.save()
-            return redirect('gbtaluka:profile')
+        if form.is_valid():
+            if form.cleaned_data['password'] == form.cleaned_data['confirm_password']:
+                user = User.objects.create_user(
+                first_name=form.cleaned_data['first_name'],
+                last_name=form.cleaned_data['last_name'],
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+                email=form.cleaned_data['email'])
+                user.set_password(form.cleaned_data['password'])
+                user_detail = UserDetail(user=user)
+                user_detail.save()
+                return redirect('gbtaluka:profile')
     else:
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
